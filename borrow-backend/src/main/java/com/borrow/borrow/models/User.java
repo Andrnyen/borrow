703b070d2b0658719632bb9551c827439c215ab1
Brian Wang <brian.wang@student.unsw.edu.au>
2023-08-31 11:48:39 +1000

    started working on connecting mongo

---
+++ borrow-backend/src/main/java/com/borrow/borrow/models/User.java
@@ -0,0 +1,11 @@
+//package com.borrow.borrow.models;
+//
+//import org.springframework.data.mongodb.core.mapping.Document;
+//
+//public class User {
+//
+//    @Id
+//    private String id;
+//
+//
+//}
