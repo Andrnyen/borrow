113241afa342c496c49472b39864a4172fe3f138
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Air.local>
2023-12-06 13:16:30 +1100

    added fetch chapters function

--- frontend/lib/api_service.dart
+++ frontend/lib/api_service.dart
@@ -29,4 +29,31 @@ class ApiService {
 
     return Book.booksFromSnapshot(temp);
   }
+
+  static Future<List<Book>> getChapters(String id) async {
+    var url = Uri.https(
+      'mangaverse-api.p.rapidapi.com',
+      '/manga/chapter',
+      {
+        'id': id
+      }
+    );
+
+    final response = await http.get(
+      url,
+      headers: {
+        'x-rapidapi-key': '1509e488c4msh055312b2b2dde14p12d522jsn9f316f4f0446',
+        'x-rapidapi-host': 'mangaverse-api.p.rapidapi.com',
+      },
+    );
+
+    Map data = jsonDecode(response.body);
+    List temp = [];
+
+    for (var i in data['data']) {
+      temp.add(i);
+    }
+
+    return Book.booksFromSnapshot(temp);
+  }
 }
